2534e44e0cd234a2558c5ec9d8b14cc95364e72c
Dinesh <dinesh.seenuvasan05@gmail.com>
2022-07-20 14:23:34 +0530

    Search Method Pending!!

--- Film_Management_System_API/Controller/FilmsController.cs
+++ Film_Management_System_API/Controller/FilmsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Film_Management_System_API.Models;
+using Film_Management_System_API.Infrastructure;
 
 namespace Film_Management_System_API.Controller
 {
@@ -13,6 +14,13 @@ namespace Film_Management_System_API.Controller
     [ApiController]
     public class FilmsController : ControllerBase
     {
+        /*private readonly IMovieRepository movieRepository;
+
+        public FilmsController(IMovieRepository movieRepository)
+        {
+            this.movieRepository= movieRepository;
+        }*/
+
         private readonly MoviesContext _context;
 
         public FilmsController(MoviesContext context)
@@ -20,25 +28,30 @@ namespace Film_Management_System_API.Controller
             _context = context;
         }
 
+
         // GET: api/Films
+       
         [HttpGet]
+        
         public async Task<ActionResult<IEnumerable<Film>>> GetFilms()
         {
-          if (_context.Films == null)
-          {
-              return NotFound();
-          }
+            if (_context.Films == null)
+            {
+                return NotFound();
+            }
             return await _context.Films.ToListAsync();
         }
 
-        // GET: api/Films/5
+
+       // GET: api/Films/5
         [HttpGet("{id}")]
+        
         public async Task<ActionResult<Film>> GetFilm(decimal id)
         {
-          if (_context.Films == null)
-          {
-              return NotFound();
-          }
+            if (_context.Films == null)
+            {
+                return NotFound();
+            }
             var film = await _context.Films.FindAsync(id);
 
             if (film == null)
@@ -48,26 +61,47 @@ namespace Film_Management_System_API.Controller
 
             return film;
         }
-        /*[HttpGet("{name}")]
-        public async Task<ActionResult<Category>> GetFilmByName(string name)
+        
+        [HttpGet("{lang}")]
+       
+        public ActionResult<string> GetFilmByLanguage(string lang) 
         {
-            if (_context.Categories == null)
+            
+           var fil  = _context.Films
+     .FromSqlRaw("EXECUTE dbo.spGetFilmByLanguage")
+     .ToList();
+            return Ok(fil);
+        }
+        /* [HttpGet("{Language}")]
+
+
+        /*[HttpGet("{search}")]
+        public async Task<ActionResult<IEnumerable<Film>>> Search(string name, Film? fil)
+
+        {
+            try
             {
+                var result = await movieRepository.Search(name, fil);
+
+                if (result.Any())
+                {
+                    return Ok(result);
+                }
+
                 return NotFound();
             }
-            var category = await _context.Categories.FindAsync(name);
-
-            if (category == null)
+            catch (Exception)
             {
-                return NotFound();
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
             }
-
-            return category;
         }*/
 
         // PUT: api/Films/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [Route("ModifyFilmsById")]
         [HttpPut("{id}")]
+        
         public async Task<IActionResult> PutFilm(decimal id, Film film)
         {
             if (id != film.FilmId)
@@ -97,8 +131,11 @@ namespace Film_Management_System_API.Controller
         }
 
         // POST: api/Films
+
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [Route("PostFilmById")]
         [HttpPost]
+    
         public async Task<ActionResult<Film>> PostFilm(Film film)
         {
           if (_context.Films == null)
@@ -112,7 +149,9 @@ namespace Film_Management_System_API.Controller
         }
 
         // DELETE: api/Films/5
+        [Route("DeleteFilmById")]
         [HttpDelete("{id}")]
+       
         public async Task<IActionResult> DeleteFilm(decimal id)
         {
             if (_context.Films == null)
